616242a00b34be4292a9a41609743a9f9c2ec975
Erick Borja <elborjas26@gmail.com>
2024-12-11 22:38:52 -0500

    se modifico comentarios de 04-archivo.py

--- dia4/04-archivos.py
+++ dia4/04-archivos.py
@@ -1,13 +1,13 @@
-file = open('alumnos.txt', 'w') #para escribir
+file = open('alumnos.txt', 'w') #para crear y escribir un archivo
 file.write('100,cesar,cesar@gmail.com')
 file.close()
 
-file = open('alumnos.txt', 'a') #para agregar
+file = open('alumnos.txt', 'a') #para agregar al archivo
 file.write('\n')
 file.write('200,ana,anar@gmail.com')
 file.close()
 
-file_read= open('alumnos.txt', 'r') #para leer
+file_read= open('alumnos.txt', 'r') #para leer el archivo
 alumnos= file_read.read()
 print(alumnos)
 print(type(alumnos))
