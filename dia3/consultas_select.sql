9f9d0214edd9df9dd94a1b46696ace347c5402ea
Erick Borja <elborjas26@gmail.com>
2025-01-10 02:29:56 -0500

    select

---
+++ dia3/consultas_select.sql
@@ -0,0 +1,11 @@
+-- Active: 1736493026762@@127.0.0.1@3306@datag3
+
+-- SELECT
+SELECT * from empleado;
+SELECT nombre,pais from empleado;
+SELECT * from empleado limit 10;
+SELECT * from empleado order by nombre;
+SELECT * from empleado order by salario DESC;
+SELECT * from empleado where pais = 'Peru';
+select * from empleado where salario > 5000;
+SELECT *from empleado where pais = 'Peru' and salario > 5000;
