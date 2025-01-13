9b30ddf317bfafe49854111f9a8c874455c8c3b0
Erick Borja <elborjas26@gmail.com>
2025-01-13 01:12:34 -0500

    dia4

---
+++ dia4/empresas/README.md
@@ -0,0 +1,6 @@
+##  paso 1 :  crear entorno virtual
+python -m venv venv
+##  paso 2 : activar el entorno virtual
+source venv/Scripts/activate
+##  paso 3 : instalar dependencias
+pip install -r requirements.txt
---
+++ dia4/empresas/db_proyecto.sql
@@ -0,0 +1,19 @@
+CREATE TABLE empresa(  
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    ruc VARCHAR(20),
+    razon_social VARCHAR(255)
+)
+
+CREATE TABLE ciudad(
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(255)
+)
+
+CREATE TABLE direccion(
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    direccion VARCHAR(255),
+    ciudad_id int,
+    empresa_id int,
+    Foreign Key (ciudad_id) REFERENCES ciudad(id),
+    Foreign Key (empresa_id) REFERENCES empresa(id)
+)
