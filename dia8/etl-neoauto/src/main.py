e8b83d17014f499f66242d4f977b5919723a4c7b
Erick Borja <elborjas26@gmail.com>
2025-01-14 14:24:53 -0500

    etl neoauto

--- dia8/etl-neoauto/src/main.py
+++ dia8/etl-neoauto/src/main.py
@@ -1,9 +1,11 @@
-from tasks.task_prueba import task_prueba
 from prefect import flow
+from tasks.task_prueba import task_prueba
+from tasks.task_extract_neoauto import task_extract_neoauto
 
 @flow(name="ETL Prueba")
 def main_flow():
-    task_prueba()
-    
+    #task_prueba()
+    autos = task_extract_neoauto()
+    print(autos)
 if __name__ == "__main__":
     main_flow()
